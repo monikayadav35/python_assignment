5af60f732eb951d19764e77288d8ed2197d64736
monikayadav35 <monikayadaqv>
2026-02-05 12:24:06 +0530

    this is my new assignment

---
+++ chapter2.py
@@ -0,0 +1,55 @@
+#age= int( input("enter your age"))
+#if age>18:
+   # print('you are eligible for vote')
+
+
+#if n>0:
+        #print('number is positive')
+#else:
+ #   print("number is negative")
+
+
+
+# if n>100:
+#     print("number is greter than 100")
+# else:
+#     print('number is less than 100')
+
+
+
+n=int(input("enter number:-"))
+n>0
+n%2==0
+n%3==0
+if n>0 or n%2==0 or n%3==0:
+    print("n greter than 0")
+    print("even")
+    print("disible")
+else:
+    print("none")
+    
+
+
+
+
+
+
+
+
+# a=5
+# b=7
+# c=9
+# if a>b:
+#     if a>c:
+#         print("a is big number") 
+#     else:
+#         if b>a:
+#             if b>c:
+#                 print("b is big number") 
+# else:
+#     if c>a:
+#         if c>b:
+#             print("c is big")
+ 
+
+
