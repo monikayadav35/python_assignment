fc14f9af19d00ee173a2b9505037a5c73bd5e9e5
monikayadav35 <monikayadaqv>
2026-02-06 18:48:16 +0530

    my new assignment

--- string.py
+++ string.py
@@ -1,247 +1,185 @@
 # ● Take a name from the user and print Hello, <name>.
 
-# name = input("Enter your name: ")
-# print("Hello," ,name)
+name = input("Enter your name: ")
+print("Hello," ,name)
 
 
-
-
+#-------------------------------------------------------------------------------
 
 # ● Take two numbers as input and print their sum.
 
-# a=int(input('enter first  number'))
-# b=int(input("enter second number"))
+a=int(input('enter first  number'))
+b=int(input("enter second number"))
 
-# print("sum=",a+b)
+print("sum=",a+b)
 
-
+#-------------------------------------------------------------------------------
 
 
 
 # ● Take a number and print You entered <number>.
 
-# num=int(input("enter number"))
-# print("you entered",num)
+num=int(input("enter number"))
+print("you entered",num)
 
-
-
-
+#-------------------------------------------------------------------------------
 
 
 # ● Take first name and last name and print them in one line.
 
 
-# first_name=input("enter first name")
-# last_name=input("enter last name")
-# print(first_name,last_name)
+first_name=input("enter first name")
+last_name=input("enter last name")
+print(first_name,last_name)
 
 
-
+#-------------------------------------------------------------------------------
 
 
 # ● Take age and print Your age is <age>.
 
-# age=int(input("enter your age"))
-# print("your age",age)
+age=int(input("enter your age"))
+print("your age",age)
 
 
-
+#-------------------------------------------------------------------------------
 
 
 # ● Take two numbers and print their difference.
 
-# a=int(input('enter first number'))
-# b=int(input("enter second number"))
-# print("difference",a-b)
-
-
-
-
-
-
-
+a=int(input('enter first number'))
+b=int(input("enter second number"))
+print("difference",a-b)
+#-------------------------------------------------------------------------------
 
 # ● Take a city name and print Welcome to <city>.
 
-# city=input("enter your city")
-# print("welcome to",city)
+city=input("enter your city")
+print("welcome to",city)
 
-
-
-
-
+#-------------------------------------------------------------------------------
 
 # ● Take a number and print its square.
 
 
-# n=int(input("enter number"))
-# print("squre is :-",n*n)
+n=int(input("enter number"))
+print("squre is :-",n*n)
 
-
-
+#-------------------------------------------------------------------------------
 
 # ● Take two values and print them in reverse order.
 
 
-# a=int(input("enter a:-"))
-# b=int(input("enter b:-"))
-# print(b,a)
+a=int(input("enter a:-"))
+b=int(input("enter b:-"))
+print(b,a)
 
+#-------------------------------------------------------------------------------
 
+# ● Take a string and print it three times in separate lines.
+string=input("enter string")
+print(string)
+print(string)
+print(string)
 
+#-------------------------------------------------------------------------------
 
+# ● Take length and width and print Area is <area>.
+length=float(input("enter  a lenght"))
+width=float(input("enter a width"))
+area=length*width
+print("area is",area)
 
-
-
-
-# # ● Take a string and print it three times in separate lines.
-# string=input("enter string")
-# print(string)
-# print(string)
-# print(string)
-
-
-
-
-
-
-
-# # ● Take length and width and print Area is <area>.
-# length=float(input("enter  a lenght"))
-# width=float(input("enter a width"))
-# area=length*width
-# print("area is",area)
-
-
-
-
+#-------------------------------------------------------------------------------
 
 # ● Take a name and a course and print Name: <name>, Course: <course>.
 
-# name=input("enter your name:-")
-# course=input("enter your course:-")
-# print("name is ",name, "course", course)
+name=input("enter your name:-")
+course=input("enter your course:-")
+print("name is ",name, "course", course)
 
-
-
-
-
-
+#-------------------------------------------------------------------------------
 
 # ● Take a number and print Double of <number> is <result>.
 
-# number=int(input("enter your number"))
-# result=number*2
-# print("number is :-",number," result is :-",result)
+number=int(input("enter your number"))
+result=number*2
+print("number is :-",number," result is :-",result)
 
 
-
-
-
+#-------------------------------------------------------------------------------
 
 
 # ● Take a word and print its first and last character.
 
-# word=input("enter word")
-# print("first character",word[0])
-# print("second character",word[-1])
-
-
-
-
-
-
+word=input("enter word")
+print("first character",word[0])
+print("second character",word[-1])
+#-------------------------------------------------------------------------------
 
 # ● Take two numbers and print their product.
 
-# m=int(input("enter first number:-"))
-# n=int(input("enter second number:-"))
-# product=m*n
-# print("product is :-",product)
+m=int(input("enter first number:-"))
+n=int(input("enter second number:-"))
+product=m*n
+print("product is :-",product)
 
-
-
-
-
-
+#-------------------------------------------------------------------------------
 
 
 # ● Take a sentence and print it in a new line with You said: before it.
 
-# sentance=input("enter sentance")
-# print("you said:",sentance)
-
-
-
-
-
+sentance=input("enter sentance")
+print("you said:",sentance)
+#-------------------------------------------------------------------------------
 
 
 # ● Take a number and print Number entered: <number>.
 
-# number=int(input("enter a number"))
-# print("number entered is :-",number)
+number=int(input("enter a number"))
+print("number entered is :-",number)
 
 
-
-
-
+#-------------------------------------------------------------------------------
 
 # ● Take three values and print them in one line separated by space.
 
-# a=int(input("enter a number"))
-# b=int(input("enter b number"))
-# c=int(input("enter c number"))
-# print(" a :-",a,"b:-",b,"c:-",c)
-
-
-
-
-
+a=int(input("enter a number"))
+b=int(input("enter b number"))
+c=int(input("enter c number"))
+print(" a :-",a,"b:-",b,"c:-",c)
+#-------------------------------------------------------------------------------
 
 
 # ● Take a name and print each character on a new line.
 
-# name=input("enter name")
-# print()
+name=input("enter name")
+print()
+
+#-------------------------------------------------------------------------------
 
 
+# ● Take a number and print Square is <result>.
+
+number=int(input("enter number"))
+result=number*number
+print("squre is :-",result)
+#-------------------------------------------------------------------------------
 
 
+full_name="rahul singh"
+print(full_name[4])
 
 
-
-
-
-
-# # ● Take a number and print Square is <result>.
-
-# number=int(input("enter number"))
-# result=number*number
-# print("squre is :-",result)
-
-
-
-
-
-
-
-
-# full_name="rahul singh"
-# print(full_name[4])
-
-
-
-
-
-# name="python programming"
-# print(name[2:6])
-# print(name[7:])
-# print(name[7:10])
-# print(name[::2])
-# print(name[-16:-12])
-# print(name[-11:])
-# print(name[-11:-8])
+#-------------------------------------------------------------------------------
+name="python programming"
+print(name[2:6])
+print(name[7:])
+print(name[7:10])
+print(name[::2])
+print(name[-16:-12])
+print(name[-11:])
+print(name[-11:-8])
 
 
 
